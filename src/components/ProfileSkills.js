75fbdb580f17cf5c46555519c078aa2a631b4ad5
MayaRokach <maya.rokach@gmail.com>
2022-06-01 14:10:59 +0300

    profile
    Continuing working on the profile page

---
+++ src/components/ProfileSkills.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+
+export default function BasicChips() {
+  return (
+<Stack direction="row" spacing={3}>
+  <Chip label="photographer" color="secondary" />
+  <Chip label="animator" color="success" />
+  <Chip label="Photoshop" color="warning" />
+</Stack>
+  );
+}
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,10 +1,13 @@
 import ProfileInfo from '../components/ProfileInfo'
+import ProfileSkills from '../components/ProfileSkills'
+import ProfilePortfolio from '../components/ProfilePortfolio'
 
 export default function Profile() {
 
     return(
         <div className='ProfileContainer' sx={{height:'100%'}}>
             <ProfileInfo />
+            <ProfileSkills />
         </div>
     )
 }
