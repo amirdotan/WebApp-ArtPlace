68e78d0181f9b267243d72c50bc9802a0a14f727
orsegalovich <101465773+orsegalovich@users.noreply.github.com>
2022-06-06 23:05:11 +0300

    Fix home buttons and icons

--- src/components/SwipeLeft.js
+++ src/components/SwipeLeft.js
@@ -5,11 +5,15 @@ import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import NavigationIcon from '@mui/icons-material/Navigation';
+import CloseIcon from '@mui/icons-material/Close';
+
+
+
 
 export default function FloatingActionButtons() {
   return (
     <Fab color="secondary" aria-label="edit" sx={{position: 'absolute', bottom : 200, left: 60}} >
-        <EditIcon />
+        <CloseIcon />
     </Fab>
   );
 }
