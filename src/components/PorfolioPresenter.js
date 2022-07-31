5a443cbc19ddeb27a94138ccae743dfddb65f2d0
abrahamiben <benjamin.abrahami@mail.huji.ac.il>
2022-07-31 12:50:25 -0400

    bug fixes for profile presenting

--- src/components/PorfolioPresenter.js
+++ src/components/PorfolioPresenter.js
@@ -1,28 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { UserAuth } from '../context/Authcontext';
-import profileDb from './ProfileInfo'
 
 // when this component wraps a different component it dosent present it if the user isn't connected
 
 const PortfolioPresenter = ({portfolio_link ,children }) => {
   const { user } = UserAuth();
 //   console.log(children)
-//   console.log(portfolio_link)
+  console.log(portfolio_link)
 
-
-  const [portfolio_data, setPortfolioData] = useState([])
-//   console.log(portfolio_data)
-
-  useEffect(() => {
-      if (portfolio_link){
-          setPortfolioData(children)
-      }
-      else{
-          setPortfolioData("null")
-      }
-  }, []);
-// console.log(portfolio_data)
-return portfolio_data
+console.log(portfolio_link)
+    if (portfolio_link){
+        console.log(portfolio_link)
+        return children
+    }
+    else{
+        return 'null'
+    }
 };
 
 export default PortfolioPresenter;
